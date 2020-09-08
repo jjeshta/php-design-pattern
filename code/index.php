229b9276e842cf4c7921af6314f10e55b82c7f9c
Jeshta Bhoyedhur <jeshta@lsl.digital>
2020-09-08 20:42:55 +0400

    command implemented

--- code/index.php
+++ code/index.php
@@ -1,2 +1,65 @@
 <?php
-//details in here
+
+// What we need to implement is;
+// An interface Command
+// A class Order that implements Command interface
+// A class Waiter (invoker)
+// A class Chef (receiver)
+
+//Command the interface
+interface ICommand {
+    public function execute();
+}
+
+//Chef, the receiver
+class Chef {
+    public function cookPasta() {
+        echo "<br><strong>Chef is cooking Chicken Alfredo</strong><hr>";
+    }
+
+    public function bakeCake() {
+        echo "<br><strong>Chef is baking blueberry cheesecake</strong><hr>";
+    }
+}
+
+//Order, the concrete command
+class Order implements ICommand{
+    private Chef $chef;
+    private string $food;
+
+    public function __construct(Chef $chef, string $food){ 
+        $this->chef = $chef;
+        $this->food = $food;
+    }
+
+    public function execute() {
+        if ($this->food == "pasta") {
+            $this->chef->cookPasta();
+        } else {
+            $this->chef->bakeCake();
+        }
+    }
+}
+
+
+class Waiter implements ICommand {
+	private Order $order;
+
+    public function __construct(Order $order){ 
+		$this->order = $order;
+	}
+
+	public function execute() {
+		$this->order->execute();
+	}
+}
+
+//Client
+$chef = new Chef;
+$order = new Order($chef, "pasta");
+$waiter = new Waiter($order);
+$waiter->execute(); 
+
+$order = new Order($chef, "cake");
+$waiter = new Waiter($order);
+$waiter->execute();
