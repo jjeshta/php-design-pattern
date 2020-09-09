2f3fceecf63158a8575328a28557e6859a4cc838
Jeshta Bhoyedhur <jeshta@lsl.digital>
2020-09-09 13:17:43 +0400

    example updated

--- code/index.php
+++ code/index.php
@@ -1,12 +1,18 @@
 <?php
+//Example of singleton class
 class DataBaseConnecter { 
-				
+	// Hold the class instance.			
 	private static $obj; 
-				
+  
+  // The constructor is private
+  // to prevent initiation with outer code.
 	private final function __construct() { 
+    // The expensive process (e.g.,db connection) goes here.
 		echo __CLASS__ . " initialize only once "; 
 	} 
-	
+  
+  // The object is created from within the class itself
+  // only if the class has no instance.
 	public static function getConnect() { 
 		if (!isset(self::$obj)) { 
 			self::$obj = new DataBaseConnecter(); 
